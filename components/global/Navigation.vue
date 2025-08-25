352c099c4cd3dbe1715b10e42374b0285dff259e
it2427 <istiak.tushar.qtec@gmail.com>
2025-08-26 01:01:32 +0600

    scrollable-nav added for mobile

--- components/global/Navigation.vue
+++ components/global/Navigation.vue
@@ -1,11 +1,12 @@
 <template>
   <nav class="bg-white border-b border-light-50">
     <div class="jonopath-container">
-      <HeaderList />
+      <div class="scrollable-nav">
+        <HeaderList />
+      </div>
     </div>
   </nav>
 </template>
-
 <script setup lang="ts">
 import HeaderList from "@/components/global/lists/HeaderList.vue";
 </script>
--- components/global/menus/DesktopMenu.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <nav>
-    <h1>MainMenu Component</h1>
-  </nav>
-</template>
-
-<script setup lang="ts"></script>
