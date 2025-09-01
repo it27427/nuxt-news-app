f5142fccc5f61f9f5d0135287b6ec58711e5cfd8
it2427 <istiak.tushar.qtec@gmail.com>
2025-09-01 14:17:54 +0600

    Bangladesh and India page created

---
+++ app/pages/topics/bangladesh/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Bangladesh Page</h1>
+</template>
+
+<script lang="ts" setup></script>
---
+++ app/pages/topics/india/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>India Page</h1>
+</template>
+
+<script lang="ts" setup></script>
