1e4bcbf303305ccfa4c8de87990e2a3c45732863
it2427 <istiak.tushar.qtec@gmail.com>
2025-09-05 06:37:50 +0600

    PopularNewsSkeleton Updated

--- app/components/Skeleton/Home/PopularNewsSkeleton.vue
+++ app/components/Skeleton/Home/PopularNewsSkeleton.vue
@@ -1,19 +1,29 @@
 <template>
-  <div class="flex flex-col gap-5 animate-pulse my-6 lg:my-10">
-    <!-- Title Skeleton -->
+  <div class="flex flex-col gap-5">
+    <!-- Section Title Skeleton -->
     <div
-      class="bg-gray-200 dark:bg-gray-700 rounded-md w-1/3 h-[1.875rem] md:h-[2rem] mb-4"
+      class="bg-gray-300 dark:bg-dark-surface rounded w-1/3 animate-pulse h-[3rem] sm:h-[5.625rem] md:h-[3.75rem] lg:h-[4.875rem]"
     ></div>
 
-    <!-- List Skeleton -->
+    <!-- Popular News List Skeleton -->
     <ul
       class="grid gap-4 grid-flow-col grid-rows-5 md:grid-rows-3 md:grid-cols-2"
     >
-      <li
-        v-for="n in 5"
-        :key="n"
-        class="bg-gray-200 dark:bg-gray-700 rounded-md w-full h-[3rem] md:h-[3.75rem] lg:h-[5rem]"
-      ></li>
+      <li v-for="n in 5" :key="n">
+        <article
+          class="flex items-center gap-4 animate-pulse h-[3rem] sm:h-[5.625rem] md:h-[3.75rem] lg:h-[4.875rem]"
+        >
+          <!-- Counter Placeholder -->
+          <div
+            class="bg-gray-200 dark:bg-dark-divider w-8 h-full rounded"
+          ></div>
+
+          <!-- Title Placeholder -->
+          <div
+            class="bg-gray-200 dark:bg-dark-divider flex-1 h-full rounded"
+          ></div>
+        </article>
+      </li>
     </ul>
   </div>
 </template>
