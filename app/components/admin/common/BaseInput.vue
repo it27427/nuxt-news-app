7a93bbef12d60b7d51e721a007d2c6ef9345730a
it2427 <istiak.tushar.qtec@gmail.com>
2025-09-06 22:19:26 +0600

    Admin BaseInput Created Component Created

---
+++ app/components/admin/common/BaseInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col gap-1">
+    <label v-if="label" :for="id" class="text-sm font-medium text-gray-700">
+      {{ label }}
+    </label>
+
+    <input
+      :id="id"
+      :type="type"
+      :placeholder="placeholder"
+      v-model="modelValue"
+      class="px-4 py-2 rounded-lg border border-gray-300 dark:border-dark-divider focus:ring-2 focus:ring-green-500 focus:outline-none"
+    />
+
+    <p v-if="error" class="text-sm text-red-500">{{ error }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    id?: string;
+    type?: string;
+    label?: string;
+    placeholder?: string;
+    error?: string;
+    modelValue: string;
+  }
+
+  const props = defineProps<Props>();
+  const emit = defineEmits(['update:modelValue']);
+</script>
