9f599d328469fd5fedec0e00b6daf9ea6647e76c
it2427 <istiak.tushar.qtec@gmail.com>
2025-09-06 22:23:01 +0600

    Admin BaseForm Component Create

---
+++ app/components/admin/common/BaseForm.vue
@@ -0,0 +1,29 @@
+<template>
+  <form
+    @submit.prevent="$emit('submit')"
+    class="flex flex-col gap-5 bg-white dark:bg-dark p-8 rounded-2xl shadow-md w-full max-w-md"
+  >
+    <h2 v-if="title" class="text-2xl font-bold text-center mb-4">
+      {{ title }}
+    </h2>
+
+    <slot />
+
+    <button
+      type="submit"
+      class="w-full py-2 px-4 rounded-lg bg-blue-600 text-white font-semibold hover:bg-blue-700"
+    >
+      {{ submitText }}
+    </button>
+  </form>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    title?: string;
+    submitText?: string;
+  }
+
+  defineProps<Props>();
+  defineEmits(['submit']);
+</script>
