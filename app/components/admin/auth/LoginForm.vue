0e53f7bb36bd5bcdfa83a558f1d1716fbdb2131c
it2427 <istiak.tushar.qtec@gmail.com>
2025-09-06 22:24:36 +0600

    LoginForm Created

---
+++ app/components/admin/auth/LoginForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <BaseForm title="Login" submitText="Login" @submit="handleLogin">
+    <BaseInput
+      id="email"
+      label="Email"
+      type="email"
+      placeholder="Enter your email"
+      v-model="form.email"
+      :error="errors.email"
+    />
+
+    <BaseInput
+      id="password"
+      label="Password"
+      type="password"
+      placeholder="Enter your password"
+      v-model="form.password"
+      :error="errors.password"
+    />
+  </BaseForm>
+</template>
+
+<script setup lang="ts">
+  import BaseForm from '@/components/admin/common/BaseForm.vue';
+  import BaseInput from '@/components/admin/common/BaseInput.vue';
+
+  const form = reactive({
+    email: '',
+    password: '',
+  });
+
+  const errors = reactive({
+    email: '',
+    password: '',
+  });
+
+  function handleLogin() {
+    errors.email = !form.email ? 'Email is required' : '';
+    errors.password = !form.password ? 'Password is required' : '';
+
+    if (!errors.email && !errors.password) {
+      console.log('Login Submitted:', form);
+      // Call API here
+    }
+  }
+</script>
