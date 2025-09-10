bbc2db25386702fa504095fcd246eb1566d8454a
it2427 <istiak.tushar.qtec@gmail.com>
2025-09-11 00:37:37 +0600

    Error page updated based on error status

--- app/error.vue
+++ app/error.vue
@@ -1,37 +1,45 @@
 <template>
   <div
-    class="flex items-center justify-center text-center w-full min-h-screen bg-red-100 text-dark p-4"
+    class="flex items-center justify-center w-full min-h-screen bg-red-200 dark:bg-slate-950 p-4"
   >
-    <div class="flex flex-col items-center justify-center text-center gap-4">
+    <div class="flex flex-col items-center justify-center text-center">
       <div
         v-if="error.statusCode === 404"
-        class="flex flex-col items-center gap-2"
+        class="flex flex-col items-center gap-4"
       >
-        <h1 class="text-7xl font-bold text-dark">৪০৪</h1>
-        <p class="text-2xl font-medium text-dark mb-4">
-          দুঃখিত, আপনি যে পৃষ্ঠাটি খুঁজছেন তা নেই।
-        </p>
+        <div class="flex flex-col items-center gap-2">
+          <h1 class="text-7xl font-bold text-red-500">ত্রুটি ৪০৪</h1>
+          <p class="text-2xl font-medium text-red-500 mb-4">
+            দুঃখিত, আপনি যে পৃষ্ঠাটি খুঁজছেন সেটি খুঁজে পাওয়া যায়নি।
+          </p>
+        </div>
+
         <BaseLink
           to="/"
-          class="bg-primary text-white font-bold rounded-sm px-5 h-12 flex items-center justify-center hover:bg-primary-dark"
+          class="bg-primary text-white font-bold rounded-sm px-5 h-12 flex items-center justify-center transition-all hover:bg-primary-dark"
         >
-          <i class="fa-solid mt-0.5 mr-1 fa-arrow-left" />
+          <i class="fa-solid mr-1 fa-arrow-left" />
           হোম পেজে ফিরে যান
         </BaseLink>
       </div>
 
-      <!-- Other Errors -->
-      <div v-else class="flex flex-col items-center gap-2">
-        <h1 class="text-7xl font-semibold text-dark">
-          ত্রুটি
-          <span class="text-red-500 font-sans font-bold">
-            {{ error.statusCode }}
-          </span>
-        </h1>
-        <p class="text-2xl font-medium text-dark mb-4">{{ error.message }}</p>
+      <!-- অন্যান্য ত্রুটি -->
+      <div v-else class="flex flex-col items-center gap-4">
+        <div class="flex flex-col items-center gap-2">
+          <h1 class="text-7xl font-bold text-red-500 font-bengali">
+            ত্রুটি
+            <span class="text-7xl font-bold text-red-500 font-bengali">
+              {{ error.statusCode }}
+            </span>
+          </h1>
+          <p class="text-2xl font-medium text-red-500 mb-4">
+            {{ error.message }}
+          </p>
+        </div>
+
         <BaseLink
           to="/"
-          class="bg-primary text-white font-bold rounded-sm px-5 h-12 flex items-center justify-center hover:bg-primary-dark"
+          class="bg-primary text-white font-bold rounded-sm px-5 h-12 flex items-center justify-center transition-all hover:bg-primary-dark"
         >
           <i class="fa-solid mt-0.5 mr-1 fa-arrow-left" />
           হোম পেজে ফিরে যান
