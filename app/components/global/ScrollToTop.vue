4c53c3191c0f68d4a4e377f1c7b4697a9144c7ee
it2427 <istiak.tushar.qtec@gmail.com>
2025-09-17 04:05:40 +0600

    ScrollToTop Button Icon Changed

--- app/components/global/ScrollToTop.vue
+++ app/components/global/ScrollToTop.vue
@@ -7,26 +7,17 @@
       :class="[
         'transition-all duration-300 ease-in-out',
         isVisible
-          ? 'opacity-100 translate-y-0' // যখন দৃশ্যমান
-          : 'opacity-0 translate-y-5 pointer-events-none', // যখন অদৃশ্য
+          ? 'opacity-100 translate-y-0'
+          : 'opacity-0 translate-y-5 pointer-events-none',
       ]"
     >
       <button
         @click="scrollToTop"
-        class="bg-adGradient text-light border-0 cursor-pointer font-bold text-2xl w-10 h-10 rounded-full flex items-center justify-center shadow-lg hover:shadow-xl focus:outline-none"
+        class="bg-adGradient text-light border-0 cursor-pointer font-bold w-10 h-10 rounded-full flex items-center justify-center shadow-lg hover:shadow-xl focus:outline-none"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="white"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"
-          />
-        </svg>
+        <client-only>
+          <Icon name="guidance:up-arrow" width="28" height="28" />
+        </client-only>
       </button>
     </div>
   </div>
