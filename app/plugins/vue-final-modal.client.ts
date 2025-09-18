9bc9a355dbdf4541b12ed90aab9bf6c65ef90957
it2427 <istiak.tushar.qtec@gmail.com>
2025-09-18 07:59:06 +0600

    vue-final-modal style added in vue-modal plugin

--- app/plugins/vue-final-modal.client.ts
+++ app/plugins/vue-final-modal.client.ts
@@ -2,6 +2,7 @@
 
 import { defineNuxtPlugin } from '#app';
 import { createVfm } from 'vue-final-modal';
+import 'vue-final-modal/style.css';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vfm = createVfm();
